dd1c6180291dba276191345d55b70ce94f01e79c
Michael Vartan <michael.vartan@student.csulb.edu>
2014-08-13 00:57:48 -0700

    changed links

--- Example_files/js/site.js
+++
@@ -1,6 +0,0 @@
-document.body.onload = function () {
-	console.log("test")
-	var links = document.getElementsByTagName("a")
-	for(var i=0;i<links.length;i++)
-		links[i].onclick = function(){location.reload()}
-}
