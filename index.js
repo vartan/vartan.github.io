3edae2d64ded9ca71fa21475aa0b1402dea4fb15
Michael Vartan <michael.wayne.vartan@gmail.com>
2024-03-14 12:12:12 -0700

    draw triangles instead of lines

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ let canvas;
 let ctx;
 const SPIKE_GROWTH_PER_MS = 1;
 const PX_PER_UNIT = 50;
+const SPIKE_COLOR =  "#333";
 function onLoad() {
   canvas = document.getElementById("spike-field");
   canvas.width = document.body.offsetWidth;
   canvas.height = window.innerHeight;
   ctx = canvas.getContext("2d");
+  drawFloor();
 
   for (let i = 0; i < 10; i++) {
     const angle = (Math.random()*0.5 + 1.25) * Math.PI;
@@ -26,6 +28,11 @@ function onLoad() {
 
 
   requestAnimationFrame(tick);
+}
+
+function drawFloor() {
+  ctx.fillStyle = SPIKE_COLOR;
+  ctx.fillRect(0, canvas.height - PX_PER_UNIT, canvas.width, PX_PER_UNIT);
 }
 
 let lastTick = 0;
@@ -83,12 +90,14 @@ class Spike {
   }
 
   draw() {
-    ctx.moveTo(this.x * PX_PER_UNIT, this.y * PX_PER_UNIT);
     const end = this.getCoordinatesAtSize(this.size);
-    ctx.lineTo(end.x * PX_PER_UNIT, end.y * PX_PER_UNIT);
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = "#333";
-    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(end.x * PX_PER_UNIT, end.y * PX_PER_UNIT);
+    const baseOffset = this.getBaseOffset();
+    ctx.lineTo((this.x + baseOffset.x) * PX_PER_UNIT, (this.y + baseOffset.y) * PX_PER_UNIT)
+    ctx.lineTo((this.x - baseOffset.x) * PX_PER_UNIT, (this.y - baseOffset.y) * PX_PER_UNIT)
+    ctx.fillStyle = "#333";
+    ctx.fill();
   }
 
   getCoordinatesAtSize(size) {
@@ -96,6 +105,14 @@ class Spike {
     const x = this.x + Math.cos(this.angle) * length;
     const y = this.y + Math.sin(this.angle) * length;
     return { x: x, y: y };
+  }
+
+  getBaseOffset() {
+    const length = this.size * this.maxSize / 20;
+    const angle = this.angle + Math.PI / 4;
+    const x = Math.cos(angle) * length;
+    const y = Math.sin(angle) * length;
+    return {x: x, y: y}
   }
 
   getTotalCount() {
